bae31c67fc017c835579c2f86f3a0cd20af30d6d
vensonhub <283747923@qq.com>
2016-10-19 16:43:25 +0800

    let & var

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
-import "babel-polyfill";
+//import "babel-polyfill";
+//
+//console.log(Array.from('aaaa'));
+//
+//const sum = (a,b)=>a+b;
+//
+//let x = sum(1,2);
+//
+//console.log("babel:"+x);
 
-console.log(Array.from('aaaa'));
+//1. Let + Const 块级作用域和常量
+/*
+ let和const的出现让 JS 有了块级作用域，还可以像强类型语言一样定义常量。由于之前没有块级作用域以及 var 关键字所带来的变量提升，经常给我们的开发带来一些莫名其妙的问题。
 
-const sum = (a,b)=>a+b;
+ 下面看两个简单的demo理解。
 
-let x = sum(1,2);
+ // demo 1
+ function f1() {
+ let n = 5;
+ if (true) {
+ let n = 10;
+ }
+ console.log(n); // 5
+ }
 
-console.log("babel:"+x);
+ // demo 2
+ const PI = 3.1415;
+ console.log(PI); // 3.1415
+
+ PI = 3;
+ console.log(PI); // TypeError: "PI" is read-only
+ */
+
+
